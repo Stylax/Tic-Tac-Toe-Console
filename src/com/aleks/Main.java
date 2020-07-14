051e4e331bb0cfae0813d406a256a1a6bb76c601
Stylax <aleksanderzawadzki0@gmail.com>
2020-07-14 21:07:26 +0200

    added user input (whole board in one string for now)

--- src/com/aleks/Main.java
+++ src/com/aleks/Main.java
@@ -1,22 +1,44 @@
 package com.aleks;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Board.print();
+        Board.printBoard(UserInput.inputIntoBoard());
     }
 }
 
 class Board {
-    static char[][] boardState = {{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'X', 'X', 'O'}};
-    public static void print (){
+    public static void printBoard (char[][] boardState){
+        System.out.println("---------");
         for (int i = 0; i < 3; i++){
+            System.out.print("| ");
             for (int j = 0; j < 3; j++){
                 System.out.print(boardState[i][j] + " ");
             }
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println("---------");
+    }
+}
+
+class UserInput {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String input = scanner.nextLine();
+
+    static char[][] boardState = new char [3][3];
+
+    public static char[][] inputIntoBoard() {
+        int help = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardState[i][j] = input.charAt(help);
+                help++;
+            }
+        }
+        return boardState;
     }
 }
